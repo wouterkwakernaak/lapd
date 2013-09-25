c6bf68f7d4084c18a73aad78e794cd710c419008
wouterkwakernaak <wouter@wouterkwakernaak.nl>
2013-09-25 14:31:56 +0200

    removed unused test code

--- src/lapd/neo4j/test/BaseGraphDbTest.java
+++ src/lapd/neo4j/test/BaseGraphDbTest.java
@@ -1,54 +1,26 @@
 package lapd.neo4j.test;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.UUID;
 
 import lapd.neo4j.GraphDbValueIO;
-import lapd.neo4j.GraphDbValueInsertionVisitor;
 import lapd.neo4j.IGraphDbValueIO;
 
 import org.eclipse.imp.pdb.facts.IValueFactory;
 import org.eclipse.imp.pdb.facts.impl.fast.ValueFactory;
-import org.junit.After;
 import org.junit.Before;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.test.TestGraphDatabaseFactory;
-import org.neo4j.tooling.GlobalGraphOperations;
 
 public class BaseGraphDbTest {
 	
-	protected IValueFactory valueFactory;
-	protected GraphDatabaseService graphDb;	
-	protected GraphDbValueInsertionVisitor graphDbValueInsertionVisitor;
-	protected GlobalGraphOperations globalGraphOperations;
+	protected IValueFactory valueFactory;	
 	protected IGraphDbValueIO graphDbValueIO;
 	protected String id;
 
 	@Before
 	public void setUp() throws IOException {
-		valueFactory = ValueFactory.getInstance();
-	    graphDb = new TestGraphDatabaseFactory().newImpermanentDatabase();	    
-	    graphDbValueInsertionVisitor = new GraphDbValueInsertionVisitor(graphDb);
-	    globalGraphOperations = GlobalGraphOperations.at(graphDb);
+		valueFactory = ValueFactory.getInstance();	    
 	    graphDbValueIO = new GraphDbValueIO(false);
 	    id = UUID.randomUUID().toString();
-	}
-	
-	@After
-	public void tearDown() {
-	    graphDb.shutdown();
-	}
-	
-	protected int countNodes() {
-		int count = 0;
-		Iterator<Node> iterator = globalGraphOperations.getAllNodes().iterator();
-		while(iterator.hasNext()) {
-			count++;
-			iterator.next();
-		}
-		return count;
 	}
 
 }
