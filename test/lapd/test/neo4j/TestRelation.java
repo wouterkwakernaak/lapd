74e6dd3f45eeb39ffa61deefb2373dc9e7f62d8f
Wouter Kwakernaak <wouter@wouterkwakernaak.nl>
2013-12-18 15:22:12 +0100

    relation test now includes a cycle

--- test/lapd/test/neo4j/TestRelation.java
+++ test/lapd/test/neo4j/TestRelation.java
@@ -10,8 +10,8 @@ public class TestRelation extends BaseGraphDbTest {
 
 	@Test
 	public void testRelation() throws GraphDbMappingException {
-		IValue tuple1 = valueFactory.tuple(new IValue[] { valueFactory.integer(1), valueFactory.string("one") });
-		IValue tuple2 = valueFactory.tuple(new IValue[] { valueFactory.integer(2), valueFactory.string("two") });
+		IValue tuple1 = valueFactory.tuple(new IValue[] { valueFactory.integer(1), valueFactory.integer(2) });
+		IValue tuple2 = valueFactory.tuple(new IValue[] { valueFactory.integer(2), valueFactory.integer(1) });
 		IValue relationValue = valueFactory.set(tuple1, tuple2);
 		graphDbValueIO.write(id, relationValue);
 		assertEquals(relationValue, graphDbValueIO.read(id, relationValue.getType()));
